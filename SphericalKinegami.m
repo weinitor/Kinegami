9d5dce6da1f548848305c402fba28bdbdaf07aab
Lucien Peach <52416020+lucienpeach@users.noreply.github.com>
2021-07-21 13:55:56 -0400

    self-assignment updates
    added functionality for spherical, planar, and universal

--- SphericalKinegami.m
+++ SphericalKinegami.m
@@ -1,5 +1,5 @@
 % Kinegami Test V1 (Spherical Wrist)
-% Last Edited 6/29/2021 by Lucien Peach
+% Last Edited 7/21/2021 by Lucien Peach
 
 clear
 close all
@@ -29,8 +29,39 @@ mirror = 'on';
 triple = 'triple';
 theta_mod = [0, 0, 0, 0];
 fingertip = 'z';
+selfassign = 'true';
 
-[infostruct, TransformStruct] = Kinegami(D, r, n, JointStruct, mirror, ...
-    triple, theta_mod, fingertip);
+N = size(JointStruct, 2) - 1;
+
+% If the selfassign tag is applied, provide Oc for each joint
+if strcmp(selfassign, 'true') == 1
+    
+    TransformStruct(N+1) = struct();
+    
+    TransformStruct(1).Oc = [1, 0, 0, 0; ...
+        0, 1, 0, 0; ...
+        0, 0, 1, -0.159];
+    
+    TransformStruct(2).Oc = [0, -1, 0, 0; ...
+        0, 0, -1, 0.2377; ...
+        1, 0, 0, 0.1];
+    
+    TransformStruct(3).Oc = [0, 0, 1, -0.139; ...
+        -1, 0, 0, 0; ...
+        0, -1, 0, 0.1];    
+    
+    TransformStruct(4).Oc = [0, 0, 1, 0; ...
+        0, -1, 0, 0; ...
+        1, 0, 0, 0.2];
+    
+else
+    
+    % Otherwise, do nothing besides initialization
+    TransformStruct(N+1) = struct();
+
+end
+
+[infostruct, TransformStruct, DataNet] = Kinegami(D, r, n, JointStruct, ...
+    mirror, triple, theta_mod, fingertip, selfassign, TransformStruct);
 
 
