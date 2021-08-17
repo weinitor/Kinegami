c258909ef5d7aa9cd9910e2489fbb7f616945135
weinitor <50150425+weinitor@users.noreply.github.com>
2021-08-17 14:10:35 -0400

    Update and rename B_crease_schematic.m to Origami_Twist_crease_schematic.m

--- Origami_Twist_crease_schematic.m
+++ Origami_Twist_crease_schematic.m
@@ -11,7 +11,7 @@ h = 0.05; %[m]
 alpha = 0; %[rad]
 
 % Outputs values for x and l in [m]
-[x, l, ls] = B_creasedesign(r, n, h, alpha);
+[x, l, ls] = Origami_Twist_creasedesign(r, n, h, alpha);
 
 % Create a figure that demonstrates the crease schematic
 
@@ -20,7 +20,7 @@ h1 = 0.03; %[m]
 h2 = 0.03; %[m]
 
 % Outputs midsection for graphing
-[dataFoldB, m, lmax] = B_papercut(x, l, ls, n, h1, h2, r, h, alpha);
+[dataFoldB, m, lmax] = Origami_Twist_papercut(x, l, ls, n, h1, h2, r, h, alpha);
 axis off
 
 % Convert to DXF
